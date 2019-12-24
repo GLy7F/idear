8dc607602d38b5c1744b130775c3a0c64e3aa1a4
bosheca <bosheca@protonmail.com>
2019-12-24 19:31:09 +0300

    create ideas pages create and show

---
+++ frontend/src/Components/Ideas/Create.jsx
@@ -0,0 +1,104 @@
+import React, { Component } from 'react'
+import { Container } from 'react-bootstrap'
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+import axios from 'axios';
+import { URL, decode} from '../../helper'
+
+const ideaSchema = Yup.object().shape({
+    title: Yup.string()
+     .min(6, 'Please enter more than 6 characters')
+     .required( 'Please enter title' ),
+    description: Yup.string()
+     .max(160, 'Please enter no more than 160 characters')
+     .required('Please enter a last name')
+   });
+
+export default class Create extends Component {
+    state = {
+        title : "",
+        description : ""
+    }
+    
+    onChange
+
+    disc(){
+        const vals = this.refs.disc.value
+        this.setState({ 'disc':vals})
+    }
+
+    handleSubmit = (val) => {
+        
+        val.user = decode() 
+
+    
+        axios.post(`${URL}/allideas/new/${val.user}`, val)
+        .then(data => {
+            console.log(data)
+            if(data.status === 200){
+                // console.log("completed")
+                this.props.history.push('/Ideas')
+            }
+        })
+        .catch(err => console.log(err))
+    }
+
+    
+    render() {
+        return (
+            <div>
+                <h3>Create Task</h3>
+                
+        <h5>please write good task.</h5> 
+                <div>
+                    {/* <form>
+                        <input name="title" />
+
+                    </form> */}
+                    <Container>
+                    <Formik
+                        onSubmit={(values) => {
+                            // console.log(values)
+                            this.handleSubmit(values)
+                        }}
+                        initialValues={{
+                            title : '',
+                            description: ''
+                        }}
+                        validationSchema={ideaSchema}
+                        >
+                        {(values) => (
+                            <Form>
+                            <Field
+                                name="title"
+                                type="text"
+                                placeholder="Title of task"
+                                className="form-control" 
+                            />
+                            <ErrorMessage
+                                name="title"
+                                component="div"
+                            />
+                            
+                            <Field
+                                name="description"
+                                as="textarea"
+                                placeholder="description of task"
+                                className="form-control" 
+                            />
+                            <ErrorMessage
+                                name="description"
+                                component="div"
+                            />
+                            <button>
+                                Submit
+                            </button>
+                            </Form>
+                        )}
+                        </Formik>
+                    </Container>
+                </div>
+            </div>
+        )
+    }
+}
---
+++ frontend/src/Components/Ideas/Ideas.jsx
@@ -0,0 +1,65 @@
+import React, { Component } from "react";
+import moment from "moment";
+// import Categories from './Categories'
+import { Card, Col, Container } from "react-bootstrap";
+import Row from "react-bootstrap/Row";
+
+export default class Ideas extends Component {
+  state = {
+    api_resp: []
+  };
+
+  componentDidMount() {
+    fetch("http://localhost:5000/allideas/ideas")
+      .then(res => res.json())
+      .then(res =>
+        this.setState({
+          api_resp: res
+        })
+      );
+  }
+
+  render() {
+    const { api_resp } = this.state;
+    console.log(api_resp);
+    // all_ideas.title,all_ideas.description
+    return (
+      <div style={{ alignItems: "center", margin: "auto" }}>
+        <h3>Task to Do !</h3>
+        <h5>This is a random tasks to do , Take one of them and challenge yourself to do it.</h5> 
+        <br/>
+        <br/>
+        <Container fluid>
+          <Row>
+            {api_resp.map(all_ideas => 
+              (
+                <Col md={3} key={all_ideas._id} className="mb-3">
+                  <Card>
+                    <Card.Body>
+                      <Card.Title>
+                        <b>{all_ideas.title}</b>
+                      </Card.Title>
+                      <Card.Text>
+                      <br/><h5>{all_ideas.description}</h5>
+                        <i className="d-block">
+                        <br/>  <br/> Created by: {" "}
+                          {all_ideas.user
+                            ? all_ideas.user.first_name
+                            : "Anonymous"}{" "}
+                            <br/>
+                            <small>Last updated {moment(all_ideas.createdAt).fromNow()}</small>
+                        </i>
+                      </Card.Text>
+                      <Card.Text></Card.Text>
+                    </Card.Body>
+                  </Card>
+                </Col>
+              )
+            )}
+          </Row>
+        </Container>
+      </div>
+    );
+  }
+}
+// (api_resp != null || api_resp != undefined) ? ):null
