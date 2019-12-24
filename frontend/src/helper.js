1d9d5ae40815993155e2ada8de8ae19bbb752e35
bosheca <bosheca@protonmail.com>
2019-12-24 19:37:20 +0300

    make helper file

---
+++ frontend/src/helper.js
@@ -0,0 +1,15 @@
+import jwtDecode from 'jwt-decode';
+
+export const URL = "http://localhost:5000";
+
+export function decode (){
+
+    let user = jwtDecode(localStorage.getItem('usertoken'))
+    console.log(user)
+    return user._id
+}
+
+export const logout = (user)=>{
+    localStorage.removeItem('usertoken') // localStorage in the browser
+    window.location.replace("http://localhost:3000/");
+}
