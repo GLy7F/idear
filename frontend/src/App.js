32ab7f7c79226f9b84b99a91608771839c3f87ac
bosheca <bosheca@protonmail.com>
2019-12-24 19:24:41 +0300

    ceate nav bar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
 import "./App.css";
-
+import Navbars from "./Components/Nav/Navbars";
 
 
 function App() {
   return (
     <div className="App">
-     
+      <Navbars />
     </div>
   );
 }
